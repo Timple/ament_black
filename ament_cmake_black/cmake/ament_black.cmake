6af50200e0ac460a19387f44f08ea5760c305f7a
Tyler Weaver <tyler@picknik.ai>
2020-03-23 15:29:17 -0600

    ament_cmake_black

---
+++ ament_cmake_black/cmake/ament_black.cmake
@@ -0,0 +1,53 @@
+# Copyright 2014-2015 Picknik Robotics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Add a test to check the code for compliance with black.
+#
+# :param TESTNAME: the name of the test, default: "black"
+# :type TESTNAME: string
+# :param ARGN: the files or directories to check
+# :type ARGN: list of strings
+#
+# @public
+#
+function(ament_black)
+  cmake_parse_arguments(ARG "" "TESTNAME;CONFIG_FILE" "" ${ARGN})
+  if(NOT ARG_TESTNAME)
+    set(ARG_TESTNAME "black")
+  endif()
+
+  find_program(ament_black_BIN NAMES "ament_black")
+  if(NOT ament_black_BIN)
+    message(FATAL_ERROR "ament_black() variable 'ament_black_BIN' must not be empty")
+  endif()
+
+  set(result_file "${AMENT_TEST_RESULTS_DIR}/${PROJECT_NAME}/${ARG_TESTNAME}.xunit.xml")
+  set(cmd "${ament_black_BIN}" "--xunit-file" "${result_file}")
+  list(APPEND cmd ${ARG_UNPARSED_ARGUMENTS})
+
+  file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/ament_black")
+  ament_add_test(
+    "${ARG_TESTNAME}"
+    COMMAND ${cmd}
+    OUTPUT_FILE "${CMAKE_BINARY_DIR}/ament_black/${ARG_TESTNAME}.txt"
+    RESULT_FILE "${result_file}"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+  )
+  set_tests_properties(
+    "${ARG_TESTNAME}"
+    PROPERTIES
+    LABELS "black;linter"
+  )
+endfunction()
---
+++ ament_cmake_black/doc/index.rst
@@ -0,0 +1,43 @@
+ament_cmake_black
+========================
+
+Checks the code style of Python source files using `black
+<http://https://github.com/psf/black>`_.
+Files with the following extension is being considered:
+``.py``.
+
+
+How to run the check from the command line?
+-------------------------------------------
+
+The command line tool is provided by the package `ament_black
+<https://github.com/tylerjw/ament_black>`_.
+
+
+How to run the check from within a CMake ament package as part of the tests?
+----------------------------------------------------------------------------
+
+``package.xml``:
+
+.. code:: xml
+
+    <buildtool_depend>ament_cmake</buildtool_depend>
+    <test_depend>ament_cmake_black</test_depend>
+
+``CMakeLists.txt``:
+
+.. code:: cmake
+
+    find_package(ament_cmake REQUIRED)
+    if(BUILD_TESTING)
+      find_package(ament_cmake_black REQUIRED)
+      ament_black()
+    endif()
+
+When running multiple linters as part of the CMake tests the documentation of
+the package `ament_lint_auto <https://github.com/ament/ament_lint>`_ might
+contain some useful information.
+
+The documentation of the package `ament_cmake_test
+<https://github.com/ament/ament_cmake>`_ provides more information on testing
+in CMake ament packages.
