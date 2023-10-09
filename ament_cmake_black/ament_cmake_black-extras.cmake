667ab7f8ae3091dd829f40f65315f87b2411ff05
Ignacio Vizzo <ignacio@dexory.com>
2023-10-09 15:24:03 +0200

    Be more agressive with linting and pre-commit
    This repo is about to be released

--- ament_cmake_black/ament_cmake_black-extras.cmake
+++ ament_cmake_black/ament_cmake_black-extras.cmake
@@ -1,22 +1,20 @@
 # Copyright 2019 Picknik Robotics
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
 # copied from ament_cmake_black/ament_cmake_black-extras.cmake
-
+# cmake-format: off
 find_package(ament_cmake_test QUIET REQUIRED)
-
 include("${ament_cmake_black_DIR}/ament_black.cmake")
-
-ament_register_extension("ament_lint_auto" "ament_cmake_black"
-"ament_cmake_black_lint_hook.cmake")
+ament_register_extension("ament_lint_auto" "ament_cmake_black" "ament_cmake_black_lint_hook.cmake")
+# cmake-format: on
--- ament_cmake_black/cmake/ament_cmake_black_lint_hook.cmake
+++ ament_cmake_black/cmake/ament_cmake_black_lint_hook.cmake
@@ -1,21 +1,18 @@
 # Copyright 2015 Picknik Robotics
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
-file(GLOB_RECURSE _source_files FOLLOW_SYMLINKS
-  "*.py"
-  "*.pyi"
-)
+file(GLOB_RECURSE _source_files FOLLOW_SYMLINKS "*.py" "*.pyi")
 if(_source_files)
   message(STATUS "Added test 'black' to check Python code style")
   ament_black()
