6e8f737af07c68ce4e95ea15315b41dacdf4fdd9
Ignacio Vizzo <ignacio@dexory.com>
2023-10-09 15:33:06 +0200

    fix test

--- ament_black/test/test_pep257.py
+++ ament_black/test/test_pep257.py
@@ -19,5 +19,5 @@ from ament_pep257.main import main
 @pytest.mark.linter
 @pytest.mark.pep257
 def test_pep257():
-    rc = main(argv=["ament_clang_format", "test"])
-    assert rc == 0, "Found docblock style errors"
+    rc = main(argv=[".", "test"])
+    assert rc == 0, "Found code style errors / warnings"
