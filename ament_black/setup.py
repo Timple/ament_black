7cedf6c6c0d6517d735d18fe2b4648d8f137d4d1
Ignacio Vizzo <ignacio@dexory.com>
2023-10-09 15:38:24 +0200

    Bump release version and make it consistent

--- ament_black/setup.py
+++ ament_black/setup.py
@@ -4,7 +4,7 @@ package_name = "ament_black"
 
 setup(
     name=package_name,
-    version="0.0.4",
+    version="0.2.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/" + package_name, ["package.xml"]),
