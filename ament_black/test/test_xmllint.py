51eae95bbab04ec38e30565aa136690b39787ebe
Ignacio Vizzo <ignacio@dexory.com>
2023-10-09 15:38:53 +0200

    isort

--- ament_black/test/test_xmllint.py
+++ ament_black/test/test_xmllint.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_xmllint.main import main
 import pytest
+from ament_xmllint.main import main
 
 
 @pytest.mark.linter
